16c89f1174a630d1bc4ec49fecb2de03cd632f4b
Felipe Oliveira <felipeoliveira.contato@gmail.com>
2021-07-02 07:52:59 -0300

    Add New Keynotes

--- src/components/Keynotes/index.js
+++ src/components/Keynotes/index.js
@@ -2,7 +2,32 @@ import Image from 'next/image';
 import styles from './Keynotes.module.scss';
 
 export default function Keynotes() {
-
+    const data = [
+        { 
+            id: 1, 
+            name: "Erik Shön", 
+            job: "Managing Director @ Erlang Solutions Nordic", 
+            description: "What Can Fintech Learn from Telecom? What Are the Jobs to Be Done and What's the Right Tool For the Job?",
+            image: "/PhotoErikSchon.jpg",
+            showMore: ""
+        },
+        { 
+            id: 2, 
+            name: "Robert Virding", 
+            job: "Erlang Co-creator | Senior Systems Architect @ Erlang Solutions", 
+            description: "The Platform of the System",
+            image: "/RobertVirding.jpg",
+            showMore: "https://codesync.global/speaker/robert-virding/"
+        },
+        { 
+            id: 3, 
+            name: "Mariano Guerra", 
+            job: "Co-founder @ Instadeq", 
+            description: "The Platform of the System",
+            image: "/MarianoGuerra.jpg",
+            showMore: "https://codesync.global/speaker/mariano-guerra/"
+        },
+    ];
     return (
         <section className={styles.keynotes}>
             <div className="container">
@@ -12,24 +37,22 @@ export default function Keynotes() {
                 </div>
                 </div>
                 <div className="row">
-                <div className="col-md-3">
-                    <div className={styles.keynoteCard}>
+                {data.map((item) => (
+                <div className="col-md-4">
+                    <div className={styles.keynoteCard} key={item.id}>
                         <Image
-                        src="/PhotoErikSchon.jpg"
-                        alt="Erick Shön"
+                        src={item.image}
+                        alt={item.name}
                         width={500}
                         height={500}
                         />
-                        <h2 className={styles.title}>Erik Shön</h2>
-                        <h4 className={styles.subtitle}>Managing Director @ Erlang Solutions Nordic </h4>
-                        <p className={styles.description}> What Can Fintech Learn from Telecom? What Are the Jobs to Be Done and What's the Right Tool For the Job?</p>
+                        <h2 className={styles.title}>{item.name}</h2>
+                        <h4 className={styles.subtitle}> {item.job} </h4>
+                        <p className={styles.description}> {item.description}</p>
+                        {item.showMore.length > 0 && <a href={item.showMore} target="_blank">+ Mais</a>}
                     </div>
                 </div>
-                <div className="col-md-3 col-md-offset-1">
-                <div className="coming-soon">
-                            <h4>Em breve, mais participações confirmadas!</h4> 
-                        </div>
-                </div>
+                ))}
                 </div>
 
             </div>
