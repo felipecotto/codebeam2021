2cc800c62e794dbdff992de0838a2febe23a5fc7
Thais Avelino <t.avelino@gmail.com>
2021-08-03 21:38:42 -0300

    [add js - delete section] add js para agenda tirar call4paper e diversidade

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,11 @@ import styles from '../src/styles/Home.module.scss'
 import db from '../db.json';
 import {  FacebookShareButton, LinkedinShareButton, TwitterShareButton, EmailShareButton } from 'react-share'
 import { FacebookIcon, LinkedinIcon, TwitterIcon, EmailIcon } from 'react-share'
-import Speakers from '../src/components/Speakers';
 import Tickets from '../src/components/Tickets';
 import Keynotes from '../src/components/Keynotes';
 import Talks from '../src/components/Talks';
 import Panels from '../src/components/Panels';
-import Diversity from '../src/components/Diversity';
 import Schedule from '../src/components/Schedule';
-import Navbar from '../src/components/Navbar';
-
 
 
 export default function Home(props) {
@@ -37,83 +33,172 @@ export default function Home(props) {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet"></link>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
         <link rel="icon" href="/favicon.ico" />
-        
       </Head>
       <Header />
+
+
+      
+  
+        <section id="schedule" className={styles.schedule}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h3>Agenda</h3>
+                        <div className="days-holder">
+
+                            <a className="daytrigger" data-toggle="collapse" href="#firstDay" role="button" aria-expanded="false" aria-controls="secondDay">
+                                Dia 1 - 05 AG0 2021 - QUINTA
+                            </a>
+
+                            <div className="collapse show" id="firstDay">
+                                <div className="card card-body">
+
+                                    <table className={styles.tabcontent}>
+                                        <thead>
+                                            <tr>
+                                                <th>
+                                                    <p>Horário</p>
+                                                </th>
+                                                <th>
+                                                    <p>Track 1</p>
+                                                </th>
+                                                <th>
+                                                    <p>Track 2</p>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                          
+                                                <tr className={styles.bodyrow}>
+                                                    <td>
+                                                        <div className={styles.timeborder}>
+                                                            <p className={styles.time}>
+                                                                09.00 - 10.00
+                                                            </p>
+                                                        </div>
+                                                    </td>
+                                                    <td className={styles.aligntop}>
+                                                    <div className={styles.verticalborder}>
+
+                                                        <p className={styles.name}>
+                                                            nome aqui
+                                                        </p>
+                                                        <p className={styles.heading, styles.headingmedium}>keynote</p>
+                                                        <p className={styles.mono, styles.medium, styles.light, styles.pt00, styles.mt10}>palestra maneira</p>
+                                                        <p className={styles.gray}>
+                                                                iniciante
+                                                        </p>
+
+                                                       
+                                                        </div>
+                                                    </td>
+                                                    <td className={styles.aligntop}>
+                                                        <p className={styles.name}>
+                                                            outro nome
+                                                        </p>
+                                                        <p className={styles.heading, styles.headingmedium}>palestra</p>
+                                                        <p className={styles.mono, styles.medium, styles.light, styles.pt00, styles.mt10}> palestra mais maneira ainda </p>
+                                                        <p className={styles.gray}>
+                                                            iniciante
+                                                        </p>
+                                                    </td>
+                                                </tr>
+
+                                            ))}
+                                        </tbody>
+                                    </table>
+
+                                </div>
+                            </div>
+
+                            <a className="daytrigger" data-toggle="collapse" href="#secondDay" role="button" aria-expanded="false" aria-controls="secondDay">
+                                Dia 2 - 06 AG0 2021 - SEXTA
+                            </a>
+                            <div className="collapse show" id="secondDay">
+                                <div class="card card-body">
+
+                                    <table className={styles.tabcontent}>
+                                        <thead>
+                                            <tr>
+                                                <th>
+                                                    <p>Horário</p>
+                                                </th>
+                                                <th>
+                                                    <p>Track 1</p>
+                                                </th>
+                                                <th>
+                                                    <p>Track 2</p>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                           
+                                                <tr className={styles.bodyrow}>
+                                                    <td>
+                                                        <div className={styles.timeborder}>
+                                                            <p className={styles.time}>
+                                                                09.00 - 10.00
+                                                            </p>
+                                                        </div>
+                                                    </td>
+                                                    <td className={styles.aligntop}>
+                                                        <div className={styles.verticalborder}>
+
+                                                            <p className={styles.name}>
+                                                                thais
+                                                            </p>
+                                                            <p className={styles.heading, styles.headingmedium}>Palestra foda</p>
+                                                            <p className={styles.mono, styles.medium, styles.light, styles.pt00, styles.mt10}> unica palestra </p>
+                                                            <p className={styles.gray}>
+                                                                avançado
+                                                            </p>
+                                                        </div>
+
+                                                    </td>
+                                                    <td className={styles.aligntop}>
+                                                        <p className={styles.name}>
+                                                            thais
+                                                        </p>
+                                                        <p className={styles.heading, styles.headingmedium}>esporro</p>
+                                                        <p className={styles.mono, styles.medium, styles.light, styles.pt00, styles.mt10}>aprenda fazendo </p>
+                                                        <p className={styles.gray}>
+                                                            iniciante
+                                                        </p>
+                                                    </td>
+                                                </tr>
+
+                                            ))}
+                                        </tbody>
+                                    </table>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+
+
+
+
       <HeroBanner />
-      <Speakers />
       <Keynotes />
       <Talks />
       <Panels />
       <Schedule />
       <Tickets />
-      <Diversity />
-
-      <section className={styles.postsContainer}>
-        <div className="container">
-          <div className={styles.title}>
-            <h2>Edições Anteriores</h2>
-            <h2>2020</h2>
-          </div>
-          
-          <div className="row d-flex-wrap">
-          {props.data.slice(0,limitVideos).map((project) => {
-          return (
-            <div className="col-md-3">
-              <article className={styles.postsContainer__post} key={project.id}>
-                <Link href={`/video/${encodeURIComponent(project.id)}`}>
-                  <div style={{ marginBottom: '24px' }}>
-                    <Image
-                      src={project.img}
-                      alt="Picture of the author"
-                      width={500}
-                      height={420}/>
-                  </div>
-                </Link>
-                <Link href={`/video/${encodeURIComponent(project.id)}`}>
-                  <a>{project.palestra}</a>
-                </Link>
-                <h4>{project.nome}</h4>
-                <div className={styles.socialShare}>
-                  <p>Compartilhe</p>
-                  <FacebookShareButton 
-                    quote="Code Beam BR - A Code Beam chegou ao Brasil!"
-                    url={`https://www.codebeambr.com/video/${project.id}`}>
-                    <FacebookIcon size={32} round={true} round={false} bgStyle={{ fill : '#4EB913' }} />
-                  </FacebookShareButton>
-                  <LinkedinShareButton
-                    title="Code Beam BR - A Code Beam chegou ao Brasil!"
-                   url={`https://www.codebeambr.com/video/${project.id}`}>
-                    <LinkedinIcon size={32} round={true} round={false} bgStyle={{ fill : '#4EB913' }} />
-                  </LinkedinShareButton>
-                  <TwitterShareButton 
-                    title="Code Beam BR - A Code Beam chegou ao Brasil!" 
-                    url={`https://www.codebeambr.com/video/${project.id}`}>
-                    <TwitterIcon size={32} round={true} round={false} bgStyle={{ fill : '#4EB913' }} />
-                  </TwitterShareButton>
-                  <EmailShareButton url={`https://www.codebeambr.com/video/${project.id}`}>
-                    <EmailIcon size={32} round={true} round={false} bgStyle={{ fill : '#4EB913' }} />
-                  </EmailShareButton>
-                </div>
-              </article>
-            </div>
-          )
-        })}
-          </div>
-            {limitVideos <= 17 &&
-              <div className={styles.ShowMore}>
-              <button onClick={() => setLimitVideos(limitVideos + 4)}>
-                Ver Mais
-              </button>
-              </div>
-            } 
-        </div>
-      </section>
       <Sponsors />
       <Footer />
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-    crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </>
   )
 }
